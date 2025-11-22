0b28d22d101e28df0b004a0b42f952bdd017ce7a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-22 01:38:39 -0300

    feat: env validation

--- projects/crawlers/src/main/java/com/liclist/crawlers/App.java
+++ projects/crawlers/src/main/java/com/liclist/crawlers/App.java
@@ -2,9 +2,12 @@ package com.liclist.crawlers;
 
 import com.liclist.crawlers.di.AppComponent;
 import com.liclist.crawlers.di.DaggerAppComponent;
+import com.liclist.crawlers.modules.commons.Env;
 
 public class App {
   public static void main(String[] args) {
+    Env.check();
+
     AppComponent component = DaggerAppComponent.create();
 
     component.tceRsCrawler().run();
---
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/commons/enums/EnvEnum.java
@@ -0,0 +1,5 @@
+package com.liclist.crawlers.modules.commons.enums;
+
+public enum EnvEnum {
+  TCERS_FILE_URL;
+}
