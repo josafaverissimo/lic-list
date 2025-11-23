eeda177eb23cfa83964a9b4278908f2452cbef96
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-22 23:04:11 -0300

    feat: add biddings table

---
+++ projects/crawlers/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,33 @@
+create table liclist_biddings_sources (
+  id varchar(100) PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  created_at TIMESTAMPTZ(3) NOT NULL DEFAULT NOW()
+);
+
+create table liclist_biddings (
+  id varchar(100) PRIMARY KEY,
+  code varchar(200) NOT NULL,
+  bidding_source_id varchar(100) NOT NULL,
+  description TEXT NOT NULL,
+  amount bigint NOT NULL,
+  amount_scale smallint NOT NULL,
+  created_at TIMESTAMPTZ(3) NOT NULL DEFAULT NOW(),
+
+  FOREIGN KEY (bidding_source_id)
+    REFERENCES liclist_biddings_sources(id)
+    ON DELETE RESTRICT
+);
+
+create table liclist_biddings_items (
+  id varchar(100) PRIMARY KEY,
+  description TEXT NOT NULL,
+  bidding_id varchar(100) NOT NULL,
+  quantity int NOT NULL,
+  unit_amount bigint NOT NULL,
+  unit_amount_scale smallint NOT NULL,
+  metric_unit varchar(50) NOT NULL,
+
+  FOREIGN KEY (bidding_id)
+    REFERENCES liclist_biddings(id)
+    ON DELETE RESTRICT
+);
