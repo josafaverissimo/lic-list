3a1556ada6d91d5db3371dbf15b2bf87821c3405
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-23 01:37:25 -0300

    feat: run seeds

--- projects/crawlers/src/main/java/com/liclist/crawlers/App.java
+++ projects/crawlers/src/main/java/com/liclist/crawlers/App.java
@@ -1,15 +1,23 @@
 package com.liclist.crawlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.liclist.crawlers.di.AppComponent;
 import com.liclist.crawlers.di.DaggerAppComponent;
 import com.liclist.crawlers.modules.commons.Env;
 
 public class App {
   public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(App.class);
+
+    logger.info("Crawlers app initiated");
+
     Env.check();
 
     AppComponent component = DaggerAppComponent.create();
 
-    component.tceRsCrawler().run();
+    component.seeds().run();
+    component.crawlers().run();
   }
 }
