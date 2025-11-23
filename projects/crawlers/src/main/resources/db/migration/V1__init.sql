a94872755c7be294c8d2484544b66f8e2fe4f524
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-23 01:37:20 -0300

    chore: add unique columns

--- projects/crawlers/src/main/resources/db/migration/V1__init.sql
+++ projects/crawlers/src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,12 @@
 create table liclist_biddings_sources (
   id varchar(100) PRIMARY KEY,
-  name varchar(50) NOT NULL,
+  name varchar(50) NOT NULL UNIQUE,
   created_at TIMESTAMPTZ(3) NOT NULL DEFAULT NOW()
 );
 
 create table liclist_biddings (
   id varchar(100) PRIMARY KEY,
-  code varchar(200) NOT NULL,
+  code varchar(200) NOT NULL UNIQUE,
   bidding_source_id varchar(100) NOT NULL,
   description TEXT NOT NULL,
   amount bigint NOT NULL,
@@ -20,6 +20,7 @@ create table liclist_biddings (
 
 create table liclist_biddings_items (
   id varchar(100) PRIMARY KEY,
+  code varchar(100) NOT NULL,
   description TEXT NOT NULL,
   bidding_id varchar(100) NOT NULL,
   quantity int NOT NULL,
