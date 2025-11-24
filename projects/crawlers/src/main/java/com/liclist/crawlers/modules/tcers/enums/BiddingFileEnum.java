02612d48fc72c60a8786272a7db5d16ec9da4ce4
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-23 21:03:22 -0300

    feat: add bidding file enum

---
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/enums/BiddingFileEnum.java
@@ -0,0 +1,16 @@
+package com.liclist.crawlers.modules.tcers.enums;
+
+public enum BiddingFileEnum {
+  BIDDING("licitacao.csv");
+
+  private final String filename;
+
+  BiddingFileEnum(String filename) {
+    this.filename = filename;
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+}
