bdb8321e8228afebd71a53711a4c7d5162281d26
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-25 06:50:03 -0300

    feat: add method to get a entities map

--- projects/crawlers/src/main/java/com/liclist/crawlers/modules/commons/database/seeds/BiddingSourceSeed.java
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/commons/database/seeds/BiddingSourceSeed.java
@@ -1,6 +1,9 @@
 package com.liclist.crawlers.modules.commons.database.seeds;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +13,9 @@ import com.liclist.crawlers.modules.commons.interfaces.Seed;
 import com.liclist.crawlers.modules.commons.repositories.BiddingsSourcesRepository;
 
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
+@Singleton
 public class BiddingSourceSeed implements Seed<BiddingSource> {
   private final BiddingsSourcesRepository biddingsSourceRepository;
 
@@ -24,12 +29,18 @@ public class BiddingSourceSeed implements Seed<BiddingSource> {
   public void run() {
     logger.info("Seeding biddings sources");
 
-    for(BiddingSource biddingSource : this.getEntities()) {
+    for (BiddingSource biddingSource : this.getEntities()) {
       this.biddingsSourceRepository.getOrCreate(biddingSource);
     }
   }
 
   public List<BiddingSource> getEntities() {
     return List.of(new BiddingSource("xx1fdefqwpfx4t1cyoflvc8n", "tcers"));
+  }
+
+  public Map<String, BiddingSource> getMapEntities() {
+    return getEntities()
+        .stream()
+        .collect(Collectors.toMap(BiddingSource::getName, Function.identity()));
   }
 }
