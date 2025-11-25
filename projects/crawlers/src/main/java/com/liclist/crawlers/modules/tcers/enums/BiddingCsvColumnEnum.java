c08373431f963f377baa3a2f603cece3a0021228
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-25 06:55:41 -0300

    feat: add getMaxIndex static method

--- projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/enums/BiddingCsvColumnEnum.java
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/enums/BiddingCsvColumnEnum.java
@@ -1,9 +1,13 @@
 package com.liclist.crawlers.modules.tcers.enums;
 
+import java.util.Arrays;
+
 public enum BiddingCsvColumnEnum {
   AGENCY_CODE(0),
-  BIDDING_ID(2),
+  BIDDING_NUMBER(2),
   BIDDING_YEAR(3),
+  BIDDING_PROCESS_NUMBER(8),
+  BIDDING_MODALITY(4),
   BIDDING_DESCRIPTION(34),
   AMOUNT(43);
 
@@ -15,5 +19,13 @@ public enum BiddingCsvColumnEnum {
 
   public int getIndex() {
     return index;
+  }
+
+  public static int getMaxIndex() {
+    return Arrays
+      .stream(values())
+      .mapToInt(BiddingCsvColumnEnum::getIndex)
+      .max()
+      .orElse(0);
   }
 }
