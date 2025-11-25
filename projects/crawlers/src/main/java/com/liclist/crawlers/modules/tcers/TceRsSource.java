4ac0ef76a88772e497853d7f6d91c20ad5219f10
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-25 07:12:03 -0300

    refactor: move bidding store process to class BiddingProcessor

--- projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/TceRsSource.java
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/TceRsSource.java
@@ -8,22 +8,16 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Iterables;
-import com.google.common.primitives.Doubles;
 import com.liclist.crawlers.modules.commons.Env;
 import com.liclist.crawlers.modules.commons.enums.EnvEnum;
-import com.liclist.crawlers.modules.tcers.dtos.BiddingDto;
-import com.liclist.crawlers.modules.tcers.enums.BiddingCsvColumnEnum;
 import com.liclist.crawlers.modules.tcers.enums.BiddingFileEnum;
-import com.liclist.crawlers.modules.tcers.repositories.BiddingsRepository;
+import com.liclist.crawlers.modules.tcers.processors.BiddingProcessor;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -32,17 +26,14 @@ import jakarta.inject.Singleton;
 public class TceRsSource {
   private final Logger logger = LoggerFactory.getLogger(TceRsSource.class);
 
-  private final String COMMA_DELIMITER = ",";
-
   private final HttpClient httpClient;
 
-  private final BiddingsRepository biddingsRepository;
+  private final BiddingProcessor biddingProcessor;
 
   @Inject
-  public TceRsSource(HttpClient httpClient,
-    BiddingsRepository biddingsRepository) {
+  public TceRsSource(HttpClient httpClient, BiddingProcessor biddingProcessor) {
     this.httpClient = httpClient;
-    this.biddingsRepository = biddingsRepository;
+    this.biddingProcessor = biddingProcessor;
   }
 
   private ZipInputStream fetchFile() throws IOException, InterruptedException {
@@ -58,94 +49,6 @@ public class TceRsSource {
     return new ZipInputStream(new ByteArrayInputStream(response.body()));
   }
 
-  private BiddingDto parseRowToBiddingDto(List<String> row)
-    throws IllegalArgumentException {
-
-    final int minLength = BiddingCsvColumnEnum.getMaxIndex();
-
-    if (row.size() < minLength)
-      throw new IllegalArgumentException(
-        "Got row with size less than " + minLength);
-
-    final String agencyCode =
-      Iterables.get(row, BiddingCsvColumnEnum.AGENCY_CODE.getIndex(), null);
-
-    final String biddingModality = Iterables
-      .get(row, BiddingCsvColumnEnum.BIDDING_MODALITY.getIndex(), null);
-
-    final String biddingNumber =
-       Iterables.get(row, BiddingCsvColumnEnum.BIDDING_NUMBER.getIndex(), null);
-
-    final String biddingYear =
-      Iterables.get(row, BiddingCsvColumnEnum.BIDDING_YEAR.getIndex(), null);
-
-    final String biddingDescription = Iterables
-      .get(row, BiddingCsvColumnEnum.BIDDING_DESCRIPTION.getIndex(), null);
-
-    final String biddingAmount =
-      Iterables.get(row, BiddingCsvColumnEnum.AMOUNT.getIndex(), null);
-
-    if (agencyCode == null || biddingNumber == null || biddingYear == null
-      || biddingDescription == null) {
-      throw new IllegalArgumentException(String
-        .format(
-          "There's some null value: {agencyCode=%s, biddingId=%s, "
-            + "biddingYear=%s, biddingDescription=%s}",
-          agencyCode, biddingNumber, biddingYear, biddingDescription));
-    }
-
-    final String code = String
-      .format("%s.%s.%s-%s", agencyCode, biddingNumber, biddingYear,
-        biddingModality);
-
-    final short amountScale = 4;
-
-    Integer amount = null;
-
-    if (biddingAmount.length() != 0) {
-      Double parsedBiddingAmount = Doubles.tryParse(biddingAmount.strip());
-
-      if (parsedBiddingAmount != null)
-        amount = (int) (parsedBiddingAmount * Math.pow(10, amountScale));
-    }
-
-    return new BiddingDto(code, biddingDescription.strip(), amount,
-      amountScale);
-  }
-
-  private void storeBiddingData(BufferedReader bufferedReader)
-    throws IOException {
-    String line;
-
-    bufferedReader.readLine();
-
-    while ((line = bufferedReader.readLine()) != null) {
-      String[] row = line.split(COMMA_DELIMITER);
-
-      try {
-        BiddingDto biddingDto = parseRowToBiddingDto(List.of(row));
-
-        try (var executor =
-          Executors.newFixedThreadPool(50, Thread.ofVirtual().factory())) {
-
-          executor.submit(() -> {
-            try {
-              this.biddingsRepository.getOrCreate(biddingDto);
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
-          });
-
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-
-      } catch (IllegalArgumentException e) {
-        logger.warn(String.format("Failed to parse row: %s", e.getMessage()));
-      }
-    }
-  }
-
   public void storeData() {
     logger.info("Storing tce rs source data");
 
@@ -159,7 +62,7 @@ public class TceRsSource {
           bufferedReader =
             new BufferedReader(new InputStreamReader(zipInputStream));
 
-          this.storeBiddingData(bufferedReader);
+          this.biddingProcessor.processCsv(bufferedReader);
         }
       }
 
--- projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/repositories/BiddingsRepository.java
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/repositories/BiddingsRepository.java
@@ -3,7 +3,8 @@ package com.liclist.crawlers.modules.tcers.repositories;
 import java.util.Optional;
 
 import org.hibernate.SessionFactory;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.liclist.crawlers.modules.commons.database.seeds.BiddingSourceSeed;
 import com.liclist.crawlers.modules.commons.entities.Bidding;
 import com.liclist.crawlers.modules.tcers.dtos.BiddingDto;
@@ -15,11 +16,12 @@ import jakarta.inject.Singleton;
 public class BiddingsRepository {
   private final SessionFactory sessionFactory;
   private final BiddingSourceSeed biddingSourceSeed;
+  private final Logger logger =
+    LoggerFactory.getLogger(BiddingsRepository.class);
 
   @Inject
-  public BiddingsRepository(
-      SessionFactory sessionFactory,
-      BiddingSourceSeed biddingSourceSeed) {
+  public BiddingsRepository(SessionFactory sessionFactory,
+    BiddingSourceSeed biddingSourceSeed) {
     this.sessionFactory = sessionFactory;
     this.biddingSourceSeed = biddingSourceSeed;
   }
@@ -29,9 +31,9 @@ public class BiddingsRepository {
       final String query = "from Bidding where code = :code";
 
       Bidding data = session
-          .createSelectionQuery(query, Bidding.class)
-          .setParameter("code", code)
-          .getSingleResultOrNull();
+        .createSelectionQuery(query, Bidding.class)
+        .setParameter("code", code)
+        .getSingleResultOrNull();
 
       return Optional.ofNullable(data);
     });
@@ -45,12 +47,17 @@ public class BiddingsRepository {
 
       session.persist(bidding);
 
+      logger
+        .info(String
+          .format("Bidding #(code=%s) has been created", biddingDto.code()));
+
+
       return bidding;
     });
   }
 
   public Bidding getOrCreate(BiddingDto biddingDto) {
     return getBiddingByCode(biddingDto.code())
-        .orElseGet(() -> this.create(biddingDto));
+      .orElseGet(() -> this.create(biddingDto));
   }
 }
