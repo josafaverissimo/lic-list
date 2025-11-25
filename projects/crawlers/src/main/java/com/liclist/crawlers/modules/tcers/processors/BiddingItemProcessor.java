5efbcb64892ba128ca3987b3479fc9f633750f87
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-11-25 07:30:34 -0300

    chore: add bidding item process class

---
+++ projects/crawlers/src/main/java/com/liclist/crawlers/modules/tcers/processors/BiddingItemProcessor.java
@@ -0,0 +1,14 @@
+package com.liclist.crawlers.modules.tcers.processors;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class BiddingItemProcessor {
+  @Inject
+  public BiddingItemProcessor() {}
+
+  public void processCsv() {
+
+  }
+}
